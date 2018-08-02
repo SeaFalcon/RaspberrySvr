6233e2034b8e4415ecf33a62fd41fb1189f463cd
SeaFalcon <seafalcon323@gmail.com>
2018-08-03 00:17:49 +0900

    Node Server Create

---
+++ server.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.send();
+});
+
+app.listen(3000, () => {
+  console.log("Listening");
+});
