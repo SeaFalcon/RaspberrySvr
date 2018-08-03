53c1b7945c02b504e91b55e436f883c33897d2ec
SeaFalcon <seafalcon323@gmail.com>
2018-08-03 10:52:47 +0900

    add notice route

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.send();
+app.get("/notice", (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, () => {
-  console.log("Listening");
+app.listen(22, () => {
+  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: '+add);
+  })
+  console.log("Listening on 8888 Port...");
 });
