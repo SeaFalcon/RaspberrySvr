a3b3ddc4b699c9c97bb7ecc8141a53fc79d787fb
SeaFalcon <seafalcon323@gmail.com>
2018-08-04 23:36:31 +0900

    Set Port 3000

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 app.use(express.static('public'));
 
@@ -8,9 +9,12 @@ app.get("/notice", (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(22, () => {
+app.listen(PORT, () => {
   require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log('addr: '+add);
   })
-  console.log("Listening on 8888 Port...");
+  console.log(`Listening on ${port} Port...`);
 });
+
+// netstat -tnlp
+// kill -9 pid
