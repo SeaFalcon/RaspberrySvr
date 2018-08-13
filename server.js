6b6b309d68370187616d3e093ccfc56248756c10
SeaFalcon <seafalcon323@gmail.com>
2018-08-14 00:38:48 +0900

    file contain dir Delete

--- server.js
+++ server.js
@@ -14,34 +14,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/fileList", (req, res) => {
   var dir = req.body.path;
   var fileList = [];
-  try{
+  try {
     var files = fs.readdirSync(dir);
-  }catch(err){
-    res.json({'err': err});
+  } catch (err) {
+    res.json({ 'err': err });
   }
 
   files.forEach((file, i, arr) => {
     try {
       if (fs.statSync(path.join(dir, file)).isDirectory()) {
-        try{
+        try {
           fs.readdirSync(path.join(dir, file));
-        }catch(err){
-          fileList.push({ name: file, isDirectory: true, permission: false});
+        } catch (err) {
+          fileList.push({ name: file, isDirectory: true, permission: false });
           return;
         }
-        fileList.push({ name: file, isDirectory: true, permission: true});
+        fileList.push({ name: file, isDirectory: true, permission: true });
       } else {
-        try{
+        try {
           fs.readdirSync(path.join(dir, file));
-        }catch(err){
-          if(err.errno === -13){
-            fileList.push({ name: file, isDirectory: false, permission: false});
+        } catch (err) {
+          if (err.errno === -13) {
+            fileList.push({ name: file, isDirectory: false, permission: false });
             return;
           }
         }
-        fileList.push({ name: file, isDirectory: false, permission: true});
+        fileList.push({ name: file, isDirectory: false, permission: true });
       }
-    } catch (err) {}
+    } catch (err) { }
   });
   res.json(fileList);
 });
@@ -50,11 +50,11 @@ app.post("/fileList", (req, res) => {
 // 폴더
 app.post("/mkdir", (req, res) => {
   var name = req.body.dirName;
-  fs.mkdir(`/${name}`, err => {
-    if(err){
-      res.json({result: err});
-    }else{
-      res.json({result: 'mkdir ${name} SUCCESS'});
+  fs.mkdir(path.join(name), err => {
+    if (err) {
+      res.json({ result: err });
+    } else {
+      res.json({ result: 'mkdir ${name} SUCCESS' });
     }
   });
 });
@@ -70,14 +70,31 @@ app.post("/copyFolder", (req, res) => {
 app.post("/rmdir", (req, res) => {
   var success = [];
   var errList = [];
-  var p  = req.body.path;
+  var p = req.body.path;
   var dirList = req.body.dirList;
 
   dirList.forEach(dir => {
     try {
-      fs.rmdirSync(p + dir);
+      fs.rmdirSync(path.join(p, dir));
       success.push(`${dir} 폴더 삭제 성공!`);
-    } catch(err){
+    } catch (err) {
+      if(err.errno === -39){
+        fs.readdirSync(path.join(p, dir)).forEach(file => {
+          try {
+            file.unlinkSync(path.join(p, dir, file));
+            success.push(`${path.join(p, dir)} 폴더 내 ${file} 삭제 성공! `)
+          } catch(err){
+            errList.push(err);
+          }
+        });
+        // 폴더 내 파일 삭제 후 폴더 삭제 시도
+        try{
+          fs.rmdirSync(path.join(p, dir));
+          success.push(`${dir} 폴더 삭제 성공!`);
+        } catch (err) {
+          errList.push(err);
+        }
+      }
       errList.push(err);
     }
   });
@@ -89,6 +106,30 @@ app.post("/rmdir", (req, res) => {
 });
 
 // 파일
+app.post('/touch', (req, res) => {
+  /*
+    'r' - Open file for reading. An exception occurs if the file does not exist.
+    'r+' - Open file for reading and writing. An exception occurs if the file does not exist.
+    'rs+' - Open file for reading and writing in synchronous mode. Instructs the operating system to bypass the local file system cache.
+    'w' - Open file for writing. The file is created (if it does not exist) or truncated (if it exists).
+    'wx' - Like 'w' but fails if path exists.
+    'w+' - Open file for reading and writing. The file is created (if it does not exist) or truncated (if it exists).
+    'wx+' - Like 'w+' but fails if path exists.
+    'a' - Open file for appending. The file is created if it does not exist.
+    'ax' - Like 'a' but fails if path exists.
+    'a+' - Open file for reading and appending. The file is created if it does not exist.
+    'ax+' - Like 'a+' but fails if path exists.
+  */
+  var name = req.body.fileName;
+
+  try {
+    fs.openSync(path.join(name), 'wx');
+    res.json("생성성공");
+  } catch (err) {
+    res.json(err);
+  }
+})
+
 app.post("/showFile", (req, res) => {
   var params = req.body;
 });
@@ -101,8 +142,25 @@ app.post("/copyFile", (req, res) => {
   var params = req.body;
 });
 
-app.post("/deleteFile", (req, res) => {
-  var params = req.body;
+app.post("/unlinkFile", (req, res) => {
+  var success = [];
+  var errList = [];
+  var p = req.body.path;
+  var fileList = req.body.fileList;
+
+  fileList.forEach(file => {
+    try {
+      fs.unlinkSync(path.join(p, file));
+      success.push(`${file} 파일 삭제 성공!`);
+    } catch (err) {
+      errList.push(err);
+    }
+  });
+
+  res.json({
+    success: success,
+    err: errList
+  });
 });
 
 app.post("/osInfo", (req, res) => {
@@ -113,9 +171,9 @@ app.post("/osInfo", (req, res) => {
     type: os.type(),         // 운영체제 이름
     platform: os.platform(),     // 운영체제 플랫폼
     arch: os.arch(),         // 운영체제 아키텍처
-    release: os.release(),      // 운영체제 버전
-  })
-})
+    release: os.release()      // 운영체제 버전
+  });
+});
 
 app.listen(PORT, () => {
   require("dns").lookup(require("os").hostname(), function (err, add, fam) {
